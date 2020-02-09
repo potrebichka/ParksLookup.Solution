424e012f8f37aa7ad5493b8d56d395fce5e074cc
Nina Potrebich <32409475+potrebichka@users.noreply.github.com>
2020-02-08 18:24:57 -0800

    Delete comment in ApplicationUser

--- ParksLookup/Models/ApplicationUser.cs
+++ ParksLookup/Models/ApplicationUser.cs
@@ -6,10 +6,6 @@ namespace ParksLookup.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        // public ApplicationUser()
-        // {
-        //     this.Reviews = new HashSet<Review>();
-        // }
-        // public virtual ICollection<Review> Reviews { get; set; }
+
     }
 }
